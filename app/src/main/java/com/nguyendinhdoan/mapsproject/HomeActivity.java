94028df7d9a965c183741db42ffd04db5a0a3415
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-05 15:52:50 +0700

    Revert "add debug log"
    This reverts commit 84faf8a

--- app/src/main/java/com/nguyendinhdoan/mapsproject/HomeActivity.java
+++ app/src/main/java/com/nguyendinhdoan/mapsproject/HomeActivity.java
@@ -3,7 +3,6 @@ package com.nguyendinhdoan.mapsproject;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,7 +20,5 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // add info log
-        Log.i("Say Hello", "Hello");
     }
 }
