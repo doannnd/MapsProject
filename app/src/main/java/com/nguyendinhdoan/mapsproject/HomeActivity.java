84faf8ae466c0167b92328df361f4e0f4a746bc4
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-05 15:51:04 +0700

    add debug log

--- app/src/main/java/com/nguyendinhdoan/mapsproject/HomeActivity.java
+++ app/src/main/java/com/nguyendinhdoan/mapsproject/HomeActivity.java
@@ -3,6 +3,7 @@ package com.nguyendinhdoan.mapsproject;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,5 +21,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // add info log
+        Log.i("Say Hello", "Hello");
     }
 }
